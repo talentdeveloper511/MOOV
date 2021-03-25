03737fd60821bd2585e1427590f8da897d751eae
Christiann Carlos San Pedro <cpsanpedro@yahoo.com>
2021-03-25 11:08:31 +0800

    configure notif when logged in

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -138,6 +138,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   handleSignIn(GoogleSignInAccount account) {
     if (account == null) {
+      configurePushNotifications();
       setState(() {
         isAuth = false;
       });
